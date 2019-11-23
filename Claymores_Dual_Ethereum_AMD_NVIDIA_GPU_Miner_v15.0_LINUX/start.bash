c47c737bfb500a2af6154e0625528b393d98fedc
Wei Su <willsuwei@hotmail.com>
2019-11-23 15:04:52 -0800

    .

--- Claymores_Dual_Ethereum_AMD_NVIDIA_GPU_Miner_v15.0_LINUX/start.bash
+++ Claymores_Dual_Ethereum_AMD_NVIDIA_GPU_Miner_v15.0_LINUX/start.bash
@@ -1,2 +1,2 @@
-# ./ethdcrminer64 -epool us2.ethermine.org:4444 -ewal 0xD376b5f31E99e795ecC7ECD899b4b19658A0C6a1.linux -epsw x -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal Redhex.my -dpsw x
-./ethdcrminer64 -epool 172.65.226.101:4444 -ewal 0xD376b5f31E99e795ecC7ECD899b4b19658A0C6a1.linux -epsw x
+./ethdcrminer64 -epool 172.65.226.101:4444 -ewal 0xD376b5f31E99e795ecC7ECD899b4b19658A0C6a1.linux -epsw x -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal Redhex.my -dpsw x
+# ./ethdcrminer64 -epool 172.65.226.101:4444 -ewal 0xD376b5f31E99e795ecC7ECD899b4b19658A0C6a1.linux -epsw x
